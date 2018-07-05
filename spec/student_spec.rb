200c13ace0f52497a1d7a38a02bf271c1efa08d3
Yurina Shin <yurinas@gmail.com>
2018-07-05 22:07:24 +0000

    automatically backed up by learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -32,7 +32,6 @@ describe "Student" do
       Student.create_table
       Student.drop_table
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='students';"
-      binding.pry
       expect(DB[:conn].execute(table_check_sql)[0]).to eq(nil)
     end
   end
